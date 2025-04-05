ab70fedb6e2b4856f10fe27d2b33b70a4d5af3af
s1goG <sigogladys@gmail.com>
2025-04-05 10:09:39 +0300

    Assignment 2

---
+++ list.py
@@ -0,0 +1,23 @@
+# QN 1 and 2
+my_list = [10, 20, 30, 40] 
+
+# QN 3
+my_list.insert(1, 15)
+
+# QN 4
+my_list.extend([50, 60, 70])
+
+# QN 5
+my_list.pop()
+
+# QN 6
+my_list.sort()
+
+# QN 7
+index_value = my_list.index(30)
+
+print(index_value)
+
+
+
+
